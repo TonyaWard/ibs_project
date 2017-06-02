c5fc7589251a32afa8fc8706387724090fa57fe2
tonyaward4 <tonyaward@ums-10-157-30-46.wireless.umn.edu>
2017-06-02 13:03:21 -0500

    add new file

---
+++ code/output.dirs.r
@@ -0,0 +1,16 @@
+#Make output subdirectories
+
+#alpha div
+dir.create("alpha_div")
+
+#bet div
+dir.create("beta_div")
+
+#dif taxa
+dir.create("diff_taxa")
+
+#risk analysis
+dir.create("risk")
+
+#taxa summary
+dir.create("taxa_sums")
